05c03fb9bb36448ebfa8441a8677b4925fd952eb
abascharles <abascharlesbenard@gmail.com>
2023-10-23 15:14:24 -0500

    Second commit

--- emobilis/urls.py
+++ emobilis/urls.py
@@ -20,3 +20,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('teachers.urls')),  # Include the app's URLs
+]
